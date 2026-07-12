9457df628ff75d447c3d5f084c2eed842f819b25
eventosdprimera <eventosdprimerainv@gmail.com>
2026-07-12 19:43:29 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,8 @@
-// Configuración global de Supabase
+
+// js/config.js
 const SUPABASE_URL = 'https://kgzignpnhhdhquhilrsb.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtnemlnbnBuaGhkaHF1aGlscnNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1NTEwNjgsImV4cCI6MjA5OTEyNzA2OH0.dN1beamFkNjytmO83_QRM1lZMChhyBv35kcMtNrh81c';
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+console.log('✅ Supabase configurado');
